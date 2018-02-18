b7185fed74450eb4c753da641cd691fcfee2a9d6
Tanay Kothari <tanaykothari1998@gmail.cm>
2018-02-18 04:34:33 -0700

    Updated contract

--- contracts/Canteen.sol
+++ contracts/Canteen.sol
@@ -2,93 +2,209 @@ pragma solidity ^0.4.15;
 
 contract Canteen {
     struct Member {
-        string image;
+        string imageName;
         bool active;
     }
 
     struct Image {
         uint replicas;
+        uint deployed;
         bool active;
     }
 
     address public owner;
 
-    event MemberJoin(string);
-    event MemberLeave(string);
+    event MemberJoin(string host);
+    event MemberLeave(string host);
+    event MemberImageUpdate(string host, string image);
 
-    mapping(string => Member) memberDetails;
+    mapping(bytes32 => Member) memberDetails;
     string[] public members;
 
-    mapping(string => Image) imageDetails;
+    mapping(bytes32 => Image) imageDetails;
     string[] public images;
+    mapping (bytes32 => uint[2][]) exposedPortsForImages;
+
+    uint MULT = 100000;
 
     modifier restricted() {
         if (msg.sender == owner) _;
     }
 
-    function Canteen() {
+    function Canteen() public {
         owner = msg.sender;
     }
 
-    // Lexicographical string compare.
-    function compare(string _a, string _b) returns (int) {
-        bytes memory a = bytes(_a);
-        bytes memory b = bytes(_b);
-
-        uint minLength = a.length;
-        if (b.length < minLength) minLength = b.length;
-
-        for (uint i = 0; i < minLength; i ++)
-            if (a[i] < b[i])
-                return - 1;
-            else if (a[i] > b[i])
-                return 1;
-        if (a.length < b.length)
-            return - 1;
-        else if (a.length > b.length)
-            return 1;
-        else
-            return 0;
-    }
-
-    function addMember(string host) restricted {
-        require(!memberDetails[host].active);
+    function addMember(string host) restricted public {
+        bytes32 hashedHost = keccak256(host);
+        require(!memberDetails[hashedHost].active);
 
         members.push(host);
-        memberDetails[host] = Member("", true);
+        memberDetails[hashedHost] = Member("", true);
 
         MemberJoin(host);
+        setImageForMember(host);
     }
 
-    function removeMember(string host) restricted {
-        require(memberDetails[host].active);
-
-        memberDetails[host].active = false;
-
+    function removeMember(string host) restricted public {
+        bytes32 hashedHost = keccak256(host);
+        require(memberDetails[hashedHost].active);
+        
+        string memory affectedImage = memberDetails[hashedHost].imageName;
+        
+        imageDetails[keccak256(affectedImage)].deployed -= 1;
+        memberDetails[hashedHost] = Member("", false);
+        
         MemberLeave(host);
-
+        
+        // Need to rebalance
+        // Eg. (A, 4), (B, 4) are two images. We have 4 members, and we remove 2 
+        // We now have A A null null -> We would need A B null null 
+        rebalanceWithUnfortunateImage(affectedImage);
     }
 
-    function addImage(string name, uint replicas) restricted {
-        require(!imageDetails[name].active);
-
+    function addImage(string name, uint replicas) restricted public {
+        bytes32 hashedName = keccak256(name);
+        require(!imageDetails[hashedName].active);
+        require(bytes(name).length > 0);
+        require(replicas > 0);
+        
         images.push(name);
-        imageDetails[name] = Image(replicas, true);
+        imageDetails[hashedName] = Image(replicas, 0, true);
+        
+        // Need to rebalance
+        // Eg. (A, 4) is one image. We have 4 members. Now we add (B, 4)
+        // We now have A A A A -> We would need A B A B
+        rebalanceWithUnfortunateImage(name);
     }
 
-    function removeImage(string name) restricted {
-        require(imageDetails[name].active);
+    function removeImage(string name) restricted public {
+        bytes32 hashedName = keccak256(name);
+        require(imageDetails[hashedName].active);
 
-        imageDetails[name].active = false;
+        imageDetails[hashedName].active = false;
+
+        // Reassigns all the affected hosts to new images
+        for (uint i = 0; i < members.length; i++) {
+            Member storage member = memberDetails[keccak256(members[i])];
+            if (member.active && keccak256(member.imageName) == hashedName) {
+                member.imageName = "";
+                setImageForMember(members[i]);
+            }
+        }
+    }
+
+    function addPortForImage(string name, uint from, uint to) restricted public {
+        exposedPortsForImages[keccak256(name)].push([from, to]);
+    }
+
+     function getPortsForImage(string name) restricted public view returns (uint[2][]) {
+        return exposedPortsForImages[keccak256(name)];
     }
 
     function getMemberDetails(string host) public constant returns (string, bool) {
-        Member storage details = memberDetails[host];
-        return (details.image, details.active);
+        Member storage details = memberDetails[keccak256(host)];
+        return (details.imageName, details.active);
     }
 
-    function getImageDetails(string name) public constant returns (uint, bool) {
-        Image storage details = imageDetails[name];
-        return (details.replicas, details.active);
+    function getImageDetails(string name) public constant returns (uint, uint, bool) {
+        Image storage details = imageDetails[keccak256(name)];
+        return (details.replicas, details.deployed, details.active);
+    }
+    
+    function rebalanceWithUnfortunateImage(string newImageName) private {
+        Image storage newImage = imageDetails[keccak256(newImageName)];
+        uint currentRatio = 0;
+        uint i;
+        Member storage member;
+
+        for (i = 0; i < members.length; i++) {
+            if (newImage.deployed >= newImage.replicas) 
+                break;
+
+            member = memberDetails[keccak256(members[i])];
+            // Looking for empty hosts and filling them up
+            if (member.active && keccak256(member.imageName) == keccak256("")) {
+                member.imageName = newImageName;
+                newImage.deployed += 1;
+                currentRatio += (MULT / newImage.replicas);
+                MemberImageUpdate(members[i], newImageName);
+            }
+        }
+
+        for (i = 0; i < members.length; i++) {
+            if (newImage.deployed >= newImage.replicas) 
+                break;
+
+            member = memberDetails[keccak256(members[i])];
+            // Now we are processing those hosts which already have images on them
+            if (member.active && keccak256(member.imageName) != keccak256("")) {
+                // Only check if the machine has some other host running
+                if (keccak256(member.imageName) != keccak256(newImageName)) {
+                    Image storage image = imageDetails[keccak256(member.imageName)];
+                    uint ratio = (image.deployed * MULT) / image.replicas;
+                    // if (ratio < currentRatio + (MULT / newImage.replicas)) {
+                    if (ratio > currentRatio) {
+                        member.imageName = newImageName;
+                        newImage.deployed += 1;
+                        image.deployed -= 1;
+                        currentRatio += (MULT / newImage.replicas);
+                        MemberImageUpdate(members[i], newImageName);
+                    }
+                }
+            }
+        }
+    }
+    
+    function setImageForMember(string host) private {
+        string memory image = getNextImageToUse();
+        bytes32 hashedHost = keccak256(host);
+        bytes32 hashedImage = keccak256(image);
+        if (hashedImage == keccak256("")) {
+            return;
+        }
+        
+        // Host currently has no image, and image hasn't reached its limit yet. 
+        require(keccak256(memberDetails[hashedHost].imageName) == keccak256(""));
+        require(imageDetails[hashedImage].deployed < imageDetails[hashedImage].replicas);
+        
+        memberDetails[hashedHost] = Member(image, true);
+        imageDetails[hashedImage].deployed += 1;
+        MemberImageUpdate(host, image);
+    }
+    
+    // Selects image with lowest usage, scales equal usage of all replicas, 
+    // with respect to the ratio of the replicas required. 
+    function getNextImageToUse() private view returns (string) {
+        string memory bestImage = "";
+        uint lowestUsage = MULT;
+        
+        for (uint i = 0; i < images.length; i++) {
+            bytes32 hash = keccak256(images[i]);
+            Image storage image = imageDetails[hash];
+            
+            if (image.deployed >= image.replicas)
+                continue;
+            
+            // deployed / usage < lowestUsage -> this has lower usage
+            if (image.active && image.deployed < lowestUsage * image.replicas) {
+                lowestUsage = (image.deployed * MULT) / image.replicas;
+                bestImage = images[i];
+            }
+        }
+        
+        return bestImage;
+    }
+    
+    function hostIsRunning(Member member) private pure returns (bool) {
+        return member.active && keccak256(member.imageName) != keccak256("");
+    }
+    
+    function getMembersCount() public view returns (uint) {
+        return members.length;
+    }
+    
+    function getImagesCount() public view returns (uint) {
+        return images.length;
     }
 }
---
+++ test/canteen_test.js
@@ -0,0 +1,74 @@
+const utils = require('web3-utils')
+const BigNumber = web3.BigNumber
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should()
+
+var Canteen = artifacts.require("Canteen");
+
+contract('Canteen', accounts => { 
+  let canteen = null
+  const owner = accounts[0]
+  const purchaser = accounts[1]
+
+  beforeEach(async function() { 
+    canteen = await Canteen.new({from: owner, gas: 3000000})
+  })
+
+  it('initial state', async function () { 
+    const members = await canteen.getMembersCount()
+    const images = await canteen.getImagesCount()
+
+    members.should.bignumber.be.equal(0)
+    images.should.bignumber.be.equal(0)
+  })
+
+  describe('adding and removing members work', () => { 
+    var details;
+
+    it('test suite 1', async function () { 
+      await canteen.addMember("host1");
+      details = await canteen.getMemberDetails("host1");
+      details[0].should.be.equal("");
+      details[1].should.be.equal(true);
+
+      await canteen.addImage("img1", 2);
+      details = await canteen.getMemberDetails("host1");
+      details[0].should.be.equal("img1");
+
+      await canteen.addMember("host2");
+      details = await canteen.getMemberDetails("host2");
+      details[0].should.be.equal("img1");
+      details[1].should.be.equal(true);
+
+      await canteen.addMember("host3");
+      details = await canteen.getMemberDetails("host3");
+      details[0].should.be.equal("");
+      details[1].should.be.equal(true);
+
+      await canteen.removeMember("host2");
+      details = await canteen.getMemberDetails("host2");
+      details[0].should.be.equal("");
+      details[1].should.be.equal(false);
+      details = await canteen.getMemberDetails("host3");
+      details[0].should.be.equal("img1");
+
+      await canteen.addImage("img2", 2);
+      const images = await canteen.getImagesCount()
+      images.should.bignumber.be.equal(2);
+      details = await canteen.getMemberDetails("host1");
+      details[0].should.be.equal("img2");
+      details = await canteen.getMemberDetails("host3");
+      details[0].should.be.equal("img1");
+
+      await canteen.removeImage("img1");
+      details = await canteen.getMemberDetails("host1");
+      details[0].should.be.equal("img2");
+      details = await canteen.getMemberDetails("host3");
+      details[0].should.be.equal("img2");
+    })
+  })
+})
+
