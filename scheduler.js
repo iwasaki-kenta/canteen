a24102dd5ae2daf0b44d2581d20addc0153be8ce
Kenta Iwasaki <dranithix@gmail.com>
2018-02-18 02:58:37 -0700

    Added in image fetching/pulling.

--- scheduler.js
+++ scheduler.js
@@ -12,85 +12,100 @@ class CanteenScheduler {
 
     const docker = new Docker({socketPath: dockerPath})
 
-    this.docker = docker;
-    this.contract = contract;
-    this.account = account;
-    this.web3 = web3;
+    this.docker = docker
+    this.contract = contract
+    this.account = account
+    this.web3 = web3
 
     await this.updateScheduler('olivere/docker-test-web')
   }
 
   async updateScheduler(scheduledImage) {
-    const containers = await this.docker.listContainers()
+    this.docker.pull(scheduledImage, (err, stream) => {
+      console.log("");
 
-    console.log(`Starting up a container with the image '${scheduledImage}'.`)
+      this.docker.modem.followProgress(stream, finished.bind(this), progress)
 
-    const containerStatus = _.find(containers, {Image: scheduledImage})
+      function progress(event) {
+        console.log(`${event.status}${event.id && ` ID: ${event.id}`}`)
+      }
 
-    const scheduleImage = async () => {
-      const containers = await this.docker.listContainers()
-      const containerStatus = _.find(containers, {Image: scheduledImage})
+      async function finished() {
+        console.log("");
 
-      let container
-      if (!containerStatus) {
-        // Create a new container if not exist.
+        const containers = await this.docker.listContainers()
 
-        container = await this.docker.createContainer({
-          Image: scheduledImage,
-          ExposedPorts: {
-            '10000/tcp': {}
-          },
-          HostConfig: {
-            PortBindings: {
-              '10000/tcp': [{HostPort: '10000'}]
-            }
+        console.log(`Starting up a container with the image '${scheduledImage}'.`)
+
+        const containerStatus = _.find(containers, {Image: scheduledImage})
+
+        const scheduleImage = async () => {
+          const containers = await this.docker.listContainers()
+          const containerStatus = _.find(containers, {Image: scheduledImage})
+
+          let container
+          if (!containerStatus) {
+            // Create a new container if not exist.
+
+            container = await this.docker.createContainer({
+              Image: scheduledImage,
+              ExposedPorts: {
+                '10000/tcp': {}
+              },
+              HostConfig: {
+                PortBindings: {
+                  '10000/tcp': [{HostPort: '10000'}]
+                }
+              }
+            })
+
+            console.log('Successfully created a new container and binded it to the scheduler.')
+          } else {
+            // Get reference to the container.
+
+            container = this.docker.getContainer(containerStatus['Id'])
+            console.log('Found a stopped container; started it and binded it to the scheduler.')
           }
-        })
 
-        console.log('Successfully created a new container and binded it to the scheduler.')
-      } else {
-        // Get reference to the container.
+          // Wipe out the old container.
+          const oldContainer = this.container
+          if (oldContainer) {
+            console.log('Stopping and removing prior image binded to the scheduler.')
+            await oldContainer.stop()
+            await oldContainer.remove()
+          }
 
-        container = this.docker.getContainer(containerStatus['Id'])
-        console.log('Found a stopped container; started it and binded it to the scheduler.')
+          // Run the new (or paused) container.
+          await container.start()
+
+          console.log('Node and scheduler is ready.')
+
+          this.container = container
+        }
+
+        if (containerStatus && containerStatus.State === 'running') {
+          let container = this.docker.getContainer(containerStatus['Id'])
+          await container.stop()
+          await container.remove()
+
+          console.log(`Found an existing running container; removing it...`)
+
+          setTimeout(async () => await scheduleImage(), 3000)
+        } else {
+          await scheduleImage()
+        }
       }
-
-      // Wipe out the old container.
-      const oldContainer = this.container
-      if (oldContainer) {
-        console.log('Stopping and removing prior image binded to the scheduler.')
-        await oldContainer.stop()
-        await oldContainer.remove()
-      }
-
-      // Run the new (or paused) container.
-      await container.start()
-
-      console.log('Node and scheduler is ready.')
-
-      this.container = container
-    }
-
-    if (containerStatus && containerStatus.State === 'running') {
-      let container = this.docker.getContainer(containerStatus['Id'])
-      await container.stop()
-      await container.remove()
-
-      console.log(`Found an existing running container; removing it...`)
-
-      setTimeout(async () => await scheduleImage(), 3000)
-    } else {
-      await scheduleImage()
-    }
-
+    })
   }
 
   async cleanup() {
-    console.log('Scheduler stopping; stopping and removing binded container.');
+    console.log('Scheduler stopping; stopping and removing binded container.')
 
     if (this.container) {
-      await this.container.stop()
-      await this.container.remove()
+      await
+        this.container.stop()
+      await
+        this.container.remove()
     }
   }
 }
